62fa7682779c72469a3b88796f1911d32951f961
GFranciska <36487318+Fanni98@users.noreply.github.com>
2021-02-22 21:38:18 +0100

    Update jnh0eq.sql

--- jnhqe0_0216/jnh0eq.sql
+++ jnhqe0_0216/jnh0eq.sql
@@ -35,6 +35,35 @@ select * from egysegek;
 select * from komponens;
 
 -- 3.feladat 
+                                                               
 create table tanfolyam(tkod int primary key, ar int, tipus varchar(30), megnevezes varchar(30));
 create table resztvevo(tajszam int primary key, nev varchar(30), lakcim varchar(30));
 create table befizetes(tkod int, foreign key(tkod) references tanfolyam(tkod), tajszam int, foreign key(tajszam) references resztvevo(tajszam));
+
+-- 6. feladat
+                                                                                                                                      
+create table konyv(isbn varchar(20) primary key, cim varchar(40), targy varchar(30), ar int);
+insert into konyv values('12f','cim1','targy1',2000);
+insert into konyv values('13a','cim2','targy2',2200);
+insert into konyv values('45y','cim3','targy3',4800);
+insert into konyv values('96n','cim4','targy4',3200);
+insert into konyv values('34c','cim5','AB',200);
+insert into konyv values('54b','cim6','AB',800);
+insert into konyv values('50o','cim7','AB',1000);
+
+select cim from konyv;
+select * from konyv where ar>2000;
+select cim from konyv where ar<1000;
+select targy from konyv;
+select cim, ar from konyv where targy='AB';
+                                                                                                                                      
+-- 7.feladat
+                                                                                                                                      
+select COUNT(*) from konyv;
+select avg(ar) from konyv;
+select MIN(ar) as legolcsobb from konyv;
+select MAX(ar) as legdragabb from konyv;
+select COUNT(*) from konyv where targy='AB';
+select MAX(ar) as legdragabbAB from konyv where targy='AB';
+select cim from konyv where ar > (select avg(ar) from konyv);
+select COUNT(*) from konyv where ar > (select avg(ar) from konyv);
